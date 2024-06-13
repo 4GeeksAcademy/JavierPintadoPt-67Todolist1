164316aaefbb5dd28a0acc5e4ff273c2045a9e90
javier5pe <72091566+javier5pe@users.noreply.github.com>
2024-06-13 09:01:17 +0000

    commit

--- src/js/component/Todolist.jsx
+++ src/js/component/Todolist.jsx
@@ -6,8 +6,27 @@ const Todolist = () =>{
     const user="Javi";
 
     const [tareas, setTareas] = useState([])
+    
+    
+    // funcion que nos crea el ususario Javi metodo POST
+    async function  crearUsuario (){
+        const uri = `${host}/users/${user}`
+        const options = {method: "POST"}
+        const response = await fetch(uri, options);
+       
+        //manejo errores
+        if (!response.ok) {
+            console.log("error",response.status,response.statusText);
+
+        };
+        const data = await response.json();
+    };
 
 
+
+
+
+    //funcion que nos trae las tareas metodo GET
     async function  traerTareas (){
         const uri = `${host}/users/${user}`
         const options = {method: "GET"}
@@ -25,17 +44,17 @@ const Todolist = () =>{
     };
 
     useEffect(()=>{
+        crearUsuario();
         traerTareas();
     },[]);
 
-    console.log(tareas);
-
 
     return (
-        <div>
-            {tareas.map((item)=>
-                <p>{item.label}</p>
-            )}
+        <div className="container text-center  col-4 mt-3clae">
+                <ul className="list-group">
+                    {tareas.map((item) =>
+                    <li className="list-group-item">{item.label}</li> )}
+                </ul>
         </div>
     )
 
