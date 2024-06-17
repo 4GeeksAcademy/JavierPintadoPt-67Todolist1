70dd41771965b6fa94c9c5341ec7dc3904cb1628
javier5pe <72091566+javier5pe@users.noreply.github.com>
2024-06-17 18:32:07 +0000

    c

--- src/js/component/Todolist.jsx
+++ src/js/component/Todolist.jsx
@@ -3,9 +3,13 @@ import React, { useEffect, useState } from  "react";
 
 const Todolist = () =>{
     const host="https://playground.4geeks.com/todo";
-    const user="Javi";
+    const user="Javi1";
 
-    const [tareas, setTareas] = useState([])
+    const [tareas, setTareas] = useState([]);
+    const [nuevaTarea, setNuevaTarea] = useState("");
+    const [borrarTarea, setBorrarTarea] = useState();
+    
+
     
     
     // funcion que nos crea el ususario Javi metodo POST
@@ -22,6 +26,28 @@ const Todolist = () =>{
         const data = await response.json();
     };
 
+    // funcion que crea tareas metodo POST
+    async function  crearTareas (){
+        const uri = `${host}/todos/${user}`;
+        const todo = {label: nuevaTarea,is_done:false};
+        const options = {
+            method: "POST",
+            headers: {"Content-type":"application/json"},
+            body: JSON.stringify(todo)
+        };
+
+        
+        const response = await fetch(uri, options);
+       
+        //manejo errores
+        if (!response.ok) {
+            console.log("error",response.status,response.statusText);
+
+        };
+        setNuevaTarea("");
+        traerTareas();
+    };
+
 
 
 
@@ -31,13 +57,26 @@ const Todolist = () =>{
         const uri = `${host}/users/${user}`
         const options = {method: "GET"}
         const response = await fetch(uri, options);
-       
-        //manejo errores
-        if (!response.ok) {
-            console.log("error",response.status,response.statusText);
 
+
+        //Funcion que borra las tareas (Metodo delete)
+        async function borrarTarea() {
+            const uri = `${host}/todos/${user}/${id}`;
+            const options = { method: "DELETE" };
+            const response = await fetch(uri, options);
         };
+    
 
+        
+
+            //manejo errores
+            if (!response.ok) {
+                console.log("error",response.status,response.statusText);
+
+            };
+       
+       
+    
         const data = await response.json();
         setTareas(data.todos);
 
@@ -48,12 +87,23 @@ const Todolist = () =>{
         traerTareas();
     },[]);
 
-
+    // html 
     return (
         <div className="container text-center  col-4 mt-3clae">
+            <input 
+            type="text"
+            value={nuevaTarea}
+            placeholder="Escribe la tarea"
+            onChange={(evento)=>setNuevaTarea(evento.target.value)}
+             />
+            <button onClick={()=>crearTareas()}>Crear Tarea</button>
                 <ul className="list-group">
                     {tareas.map((item) =>
-                    <li className="list-group-item">{item.label}</li> )}
+                    <div>
+                    <li className="list-group-item">{item.label}</li> 
+                    <button onClick={() => borrarTarea(item.id)}>Borrar</button>
+                    </div>)}
+
                 </ul>
         </div>
     )
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,6 +9,7 @@ const Home = () => {
 		<div className="text-center">
 			<h1 className="text-center mt-5">Tododlist</h1>
 			<Todolist/>
+			
 		</div>
 	);
 };
